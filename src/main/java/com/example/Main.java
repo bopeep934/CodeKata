83b2628b1b9348c520627ec50fb8250bf467ac5b
bopeep934 <gkinfn@gmail.com>
2025-02-07 09:35:31 +0900

    37번_정답

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -3,10 +3,16 @@ package com.example;
 public class Main {
     public static void main(String[] args) {
 
-        Solution36 s = new Solution36();
+        Solution37 s = new Solution37();
         //System.out.println(s.solution34("Zbcdefg"));
         //System.out.println(s.solution35(3,20,4));
-        System.out.println(s.solution36("34333"));
+        //System.out.println(s.solution36("34333"));
+        int[][] a= {{1,2},{3,4}};
+        int[][] b={{3,4},{5,6}};
+        int [][] c=s.solution37(a,b);
+        for(int[] i: c)
+            for(int j: i)
+                System.out.println(j);
     }
 
 }
