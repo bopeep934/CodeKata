8214def969557459cc05c9e42b055b5cb8bb3301
bopeep934 <gkinfn@gmail.com>
2025-03-09 01:04:57 +0900

    52

--- src/main/java/com/example/Solution52.java
+++ src/main/java/com/example/Solution52.java
@@ -2,23 +2,7 @@ package com.example;
 
 public class Solution52 {
     public int solution52(int a, int b, int n) {
-        int answer = 0;
-        int l=0;
-        int c=0;
-        c=a/b;
+        return (n - b) / (a - b) * b;
 
-        while(true){
-            answer+=n/c;
-            l=n%c;
-
-            n=n/c;
-            n+=l;
-
-            if(n<a){
-                break;
-            }
-        }
-
-        return answer;
     }
 }
