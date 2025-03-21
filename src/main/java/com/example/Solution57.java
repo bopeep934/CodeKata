85edc4d7e2d8918a60ecbb977e0eea3ac2013ef8
bopeep934 <gkinfn@gmail.com>
2025-03-21 21:54:49 +0900

    57푸는중3

--- src/main/java/com/example/Solution57.java
+++ src/main/java/com/example/Solution57.java
@@ -1,35 +1,72 @@
 package com.example;
 
-public class Solution57 {
-    public int[] solution57(int[] answers) {
-        int[] answer = {};
+import java.util.ArrayList;
+import java.util.Arrays;
 
-        int[] math1 = {1, 2, 3, 4, 5};
-        int[] math2 = {2, 1, 2, 3, 2, 4, 2, 5};
-        int[] math3 = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
+public class Solution57 {
+    public ArrayList<Integer> solution57(int[] answers) {
+        ArrayList<Integer> answer = new ArrayList<>();
+
+        ArrayList<Integer> math1 = new ArrayList<>(Arrays.asList(1,2,3,4,5));
+        ArrayList<Integer> math2 = new ArrayList<>(Arrays.asList(2,1,2,3,2,4,2,5));
+        ArrayList<Integer> math3 = new ArrayList<>(Arrays.asList(3,3,1,1,2,2,4,4,5,5));
+        int[] math5 = {2, 1, 2, 3, 2, 4, 2, 5};
+        int[] math6 = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
 
         int a = 0;
         int b = 0;
         int c = 0;
 
         for(int i=0;i<answers.length;i++){
-            for(int j=0; j<math1.length;j++){
-                if (math1[i] == answers[i])
-                    a++;
-            }
+            if(answers[i]==math1.get(i))
+                a++;
+            if(math1.get(i)==null)
+                math1.set(i,math1.get(i-(i-1)));
         }
-
-        for (int i = 0; i < answers.length; i++) {
-            if(math1.length<i)
-                a++;
-            if (math1[i] == answers[i])
-                a++;
-            if (math2[i] == answers[i])
+        for(int i=0;i<answers.length;i++){
+            if(answers[i]==math2.get(i))
                 b++;
-            if (math3[i] == answers[i])
+            if(math2.get(i)==null)
+                math2.set(i,math2.get(i-(i-1)));
+        }
+        for(int i=0;i<answers.length;i++){
+            if(answers[i]==math2.get(i))
                 c++;
+            if(math3.get(i)==null)
+                math3.set(i,math3.get(i-(i-1)));
         }
 
-        return answer;
+        if(a==b){
+            if(b==c){
+                answer.add(1);
+                answer.add(2);
+                answer.add(3);
+            }
+            if(b>c){
+                answer.add(1);
+                answer.add(2);
+            }
+            if(b<c){
+                answer.add(1);
+                answer.add(3);
+            }
+        }else if(b==c){
+            if(a>b){
+                answer.add(1);
+            }if(a<b){
+                answer.add(2);
+                answer.add(3);
+            }
+        }else if(a==c){
+            if(a<b){
+                answer.add(2);
+            }if(a>b){
+                answer.add(1);
+                answer.add(3);
+            }
+        }else if(a>b)
+
+
+
     }
 }
