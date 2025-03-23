988a654949e02ffaa3b8742878eaf7a419c34ce3
bopeep934 <gkinfn@gmail.com>
2025-03-23 20:29:17 +0900

    58푸는중

---
+++ src/main/java/com/example/Solution58.java
@@ -0,0 +1,14 @@
+package com.example;
+
+public class Solution58 {
+    public int solution58(int[] nums) {
+        int answer = -1;
+
+//        for(int i=0;i<nums.length;i++)
+//            if(num[i]+num[i+1]+num[i+2]%2==1){
+        //        answer++;
+
+
+        return answer;
+    }
+}
